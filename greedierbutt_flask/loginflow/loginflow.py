d963ee582557b70edeae8482f6a25dc0d4178778
pkprotoplasm <pkprotoplasm@greedierbutt.com>
2023-08-26 21:37:28 +0000

    Add exception handling in case of API exhuastion on login

--- greedierbutt_flask/loginflow/loginflow.py
+++ greedierbutt_flask/loginflow/loginflow.py
@@ -56,19 +56,22 @@ def login_callback():
         # Set the steamid in session.
         session["steamid"] = steamID
         
-        # Call the Steam API to get the rest of the player's profile information.
-        r = requests.get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={app.config.get('STEAM_KEY')}&steamids={steamID}")
-        steamJSON = r.json()['response']['players'][0]
+        try:
+            # Call the Steam API to get the rest of the player's profile information.
+            r = requests.get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={app.config.get('STEAM_KEY')}&steamids={steamID}")
+            steamJSON = r.json()['response']['players'][0]
 
-        # Set additional necessary session variables for frontend.
-        session["personaname"] = steamJSON["personaname"]
-        session["avatar"] = steamJSON["avatar"]
+            # Set additional necessary session variables for frontend.
+            session["personaname"] = steamJSON["personaname"]
+            session["avatar"] = steamJSON["avatar"]
 
-        session.modified = True
+            session.modified = True
 
-        # Update the player's profile stored in our database.
-        g.cursor.execute("UPDATE profiles SET personaname=%s, profileurl=%s, avatar=%s, avatarmedium=%s, avatarfull=%s WHERE steamid=%s", [steamJSON["personaname"], steamJSON["profileurl"], steamJSON["avatar"], steamJSON["avatarmedium"], steamJSON["avatarfull"], steamID])
-        dbConn.connection.commit()
+            # Update the player's profile stored in our database.
+            g.cursor.execute("UPDATE profiles SET personaname=%s, profileurl=%s, avatar=%s, avatarmedium=%s, avatarfull=%s WHERE steamid=%s", [steamJSON["personaname"], steamJSON["profileurl"], steamJSON["avatar"], steamJSON["avatarmedium"], steamJSON["avatarfull"], steamID])
+            dbConn.connection.commit()
+        except Exception as e:
+            logger.warn(f"login_callback(): Exception: {e}")
         
         flash("Thanks for logging in!", "success")
     else:
